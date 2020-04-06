b20db17c652fbda7e1e41196f5c888fae7ac9716
arthur <arthur.covo@orange.fr>
2020-04-06 15:54:55 +0200

    marchepas

--- screens/Controle.js
+++ screens/Controle.js
@@ -4,53 +4,89 @@ import cine from "../components/cine.json";
 import Progression from '../components/Progression';
 import '../screens/Controle.css';
 
-const rang=2;
 
 class Controle extends Component  {
     state = {
-        q1:true,
-        q2:true,
-        q3:true,
-        q4:true,
-        q5:true
+        rang:1,
+        q1:"",
+        q2:"",
+        q3:"",
+        q4:"",
+        q5:""
       }
 
     constructor(props) {
         super(props);
+        var self=this;
 
-        var self = this;
 
         this._q1handleChange = this._q1handleChange.bind(this);
         this._q2handleChange = this._q2handleChange.bind(this);
         this._q3handleChange = this._q3handleChange.bind(this);
         this._q4handleChange = this._q4handleChange.bind(this);
         this._q5handleChange = this._q5handleChange.bind(this);
-
-
+        this.changerang = this.changerang.bind(this);
         
       } 
 
       _q1handleChange(event) {
-        this.setState({ q1: event.target.value});
+          if (event.target.value==="true")
+          {this.setState({ q1:true });}
+          else {
+            this.setState({ q1:false });
+          }
+                
       }
       _q2handleChange(event) {
-        this.setState({ q2: event.target.value});
-      }
-      _q3handleChange(event) {
-        this.setState({ q3: event.target.value});
-      }
-      _q4handleChange(event) {
-        this.setState({ q4: event.target.value});
-      }
-      _q5handleChange(event) {
-        this.setState({ q5: event.target.value});
-      }
+        if (event.target.value==="true")
+        {this.setState({ q2:true });}
+        else {
+          this.setState({ q2:false });
+        }
+              
+    }
+    _q3handleChange(event) {
+        if (event.target.value==="true")
+        {this.setState({ q3:true });}
+        else {
+          this.setState({ q3:false });
+        }
+              
+    }
+    _q4handleChange(event) {
+        if (event.target.value==="true")
+        {this.setState({ q4:true });}
+        else {
+          this.setState({ q4:false });
+        }
+              
+    }
+    _q5handleChange(event) {
+        if (event.target.value==="true")
+        {this.setState({ q5:true });}
+        else {
+          this.setState({ q5:false });
+        }
+              
+    }
+
+    componentDidMount(){
+        this.changerang
+    }
+    changerang(){
+        if (this.state.rang>3){
+            this.setState({rang:1});
+        }
+        else {
+            this.setState({rang:this.state.rang+1});
+        }
+
+    }
     
 
       handlePress =  async () => {
-        console.log(rang);
         
-        return fetch('http://127.0.0.1:3000/api/CINE/putsession', 
+        fetch('http://127.0.0.1:3000/api/CINE/putsession', 
         {
           method: 'POST',
           headers: {
@@ -59,39 +95,25 @@ class Controle extends Component  {
           },
           body: JSON.stringify({
             type: "C",
-            rang: 2,
+            rang: this.state.rang,
             userId: "5e88dec1cf7202b3d1b66d00",
             response: [this.state.q1,this.state.q2,this.state.q3,this.state.q4,this.state.q5]
           })
         })
-          .then((response) => response.json())
-          .then(function (res) {
-              log.console("hello")
-            self.setState({ 
-                q1: res.data[0].q1,
-                q2: res.data[0].q2,
-                q3: res.data[0].q3,
-                q4: res.data[0].q4,
-                q5: res.data[0].q5
-             });
-        })
-          .then((json) => {
-            
-            return json;
-            
-          })
-          
-          
+          .then(function(response) {
+            console.log(response)
+            this.changerang();
+          } )
 
           .catch((error) => {
             console.error(error);
-          });         
+          });  
+         
       }
 
     render(){
-        if (rang <3){
         return (cine.Controle.map((c) => {
-            if (c.id==rang){
+            if (c.id==this.state.rang){
                 return <div className="baba">
                     <div className="bar">
                         <h2> Contrôle </h2>
@@ -110,45 +132,45 @@ class Controle extends Component  {
                             <div className="qcine">
                             {"1)"+c.q1}
                             </div>
-                            <Input type="select" onChange={this._q1handleChange} id="q1" style={{ width: "20%", marginLeft:"135px"}}>
-                            <option value={true} >Oui</option>
-                            <option value={false}>Non</option>
+                            <Input type="select" onChange={this._q1handleChange} id="qu1" style={{ width: "20%", marginLeft:"135px"}}>
+                            <option value="true" >Oui</option>
+                            <option value="false">Non</option>
                             </Input>
                         </label>
                         <label > 
                             <div className="qcine">
                             {"2)"+c.q2}
                             </div>
-                            <Input type="select" onChange={this._q2handleChange} id="q2" style={{ width: "20%", marginLeft:"135px"}}>
-                            <option value={true} >Oui</option>
-                            <option value={false}>Non</option>
+                            <Input type="select" onChange={this._q2handleChange} id="qu2" style={{ width: "20%", marginLeft:"135px"}}>
+                            <option value="true" >Oui</option>
+                            <option value="false" >Non</option>
                             </Input>
                         </label>
                         <label > 
                             <div className="qcine">
                             {"3)"+c.q3}
                             </div>
-                            <Input type="select" onChange={this._q3handleChange} id="q3" style={{ width: "20%", marginLeft:"135px"}}>
-                            <option value={true} >Oui</option>
-                            <option value={false}>Non</option>
+                            <Input type="select" onChange={this._q3handleChange} id="qu3" style={{ width: "20%", marginLeft:"135px"}}>
+                            <option value="true" >Oui</option>
+                            <option value="false">Non</option>
                             </Input>
                         </label>
                         <label > 
                             <div className="qcine">
                             {"4)"+c.q4}
                             </div>
-                            <Input type="select" onChange={this._q4handleChange} id="q4" style={{ width: "20%", marginLeft:"135px"}}>
-                            <option value={true} >Oui</option>
-                            <option value={false}>Non</option>
+                            <Input type="select" onChange={this._q4handleChange} id="qu4" style={{ width: "20%", marginLeft:"135px"}}>
+                            <option value="true" >Oui</option>
+                            <option value="false">Non</option>
                             </Input>
                         </label>
                         <label > 
                             <div className="qcine">
                             {"5)"+c.q5}
                             </div>
-                            <Input type="select" onChange={this._q5handleChange} id="q5" style={{ width: "20%", marginLeft:"135px"}}>
-                            <option value={true} >Oui</option>
-                            <option value={false}>Non</option>
+                            <Input type="select" onChange={this._q5handleChange} id="qu5" style={{ width: "20%", marginLeft:"135px"}}>
+                            <option value="true" >Oui</option>
+                            <option value="false">Non</option>
                             </Input>
                         </label>
 
@@ -156,7 +178,7 @@ class Controle extends Component  {
                     
                     </div>
 
-                    <Button onClick={this.handlePress.bind(this)}  color="secondary" size="lg" block style={{width:"30%",marginLeft:"130px"}}>Valider </Button>
+                    <Button onClick={this.handlePress.bind(this)}  color="secondary" size="lg" block style={{width:"30%",marginLeft:"130px"}}> Valider </Button>
 
                 </div>
                 
@@ -166,7 +188,7 @@ class Controle extends Component  {
         })
 
         )
-    }
+    
 }
     
 }
