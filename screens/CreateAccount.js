b276fbabe31196f81eb0163dc80fe09c4c4eede8
Sarah Kardache <sarah.kardache@edu.ece.fr>
2020-04-07 11:38:11 +0200

    bonne version 2

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -22,39 +22,164 @@ class CreateAccount extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-          username: '',
-          mail: '',
-           password: '' ,
-        prenom:'',
+        username: '',
+        email: '',
+        password: '' ,
+        firstname:'',
+        lastname:'',
         sexe:null,
         age:0,
-        domaine : null,
-        fonction : null,
-        source : null, 
-        echelle : null, 
-        timide: null,
-        confiance: null, 
-        oral:null,
-        traitement:null,
-        chronique:null,
-        attenuer:null,
-        moyens:null,
+        professionalSituation : '',
+        socialSituation : '',
+        children : 0 ,
+        stressFactors: '',
+        stressEscape: '',
+        stressRelief: '',
+        emotionalConseq: '',
+        physicalConseq: '',
 
         };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+      
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleChangePseudo = this.handleChangePseudo.bind(this);
+        this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
+        this.handleChangeLastName = this.handleChangeLastName.bind(this);
+       
+        this.handleChangeSexe = this.handleChangeSexe.bind(this); 
+        this.handleChangeAge = this.handleChangeAge.bind(this);
+        this.handleChangeDomaine = this.handleChangeDomaine.bind(this);
+        this.handleChangeSocial = this.handleChangeSocial.bind(this);
+        this.handleChangeChildren = this.handleChangeChildren.bind(this);
+        this.handleChangeFactors = this.handleChangeFactors.bind(this);
+        this.handleChangeEscape = this.handleChangeEscape.bind(this);
+        this.handleChangeRelief = this.handleChangeRelief.bind(this);
+        this.handleChangeEmotion = this.handleChangeEmotion.bind(this);
+        this.handleChangePhysical = this.handleChangePhysical.bind(this);
     }
   
-    handleChange(event) {
-      this.setState({value: event.target.value});
+
+    handleChangeEmail(event) {
+      this.setState({email: event.target.value});
     }
-  ///AFFICHE UN MESSAGE POUR DIRE QUE L'AUTHENTIFICATION A ETE REUSSI 
-    handleSubmit(event) {
-      alert("Authentification reussie"); 
-      event.preventDefault();
+
+    handleChangePassword(event) { 
+    this.setState({password: event.target.value });
     }
+
+    
+    handleChangePseudo(event) { 
+    this.setState({pseudo: event.target.value });
+    }
+
+    handleChangeFirstName(event) { 
+    this.setState({firstname: event.target.value });
+    }
+   
+    handleChangeLastName(event) { 
+    this.setState({firstname: event.target.value });
+    }
+
+      handleChangeSexe(event) { 
+        this.setState({sexe: event.target.value });
+      }
   
+      handleChangeAge(event) { 
+          this.setState({age: event.target.value });
+      }
+      
+      handleChangeDomaine(event) { 
+          this.setState({professionalSituation: event.target.value });
+      }
+     
+      handleChangeSocial(event) { 
+          this.setState({socialSituation: event.target.value });
+          }
+      
+      handleChangeChildren(event) { 
+          this.setState({children: event.target.value });
+      }
+   
+      handleChangeFactors(event) { 
+          this.setState({stressFactors: event.target.value });
+      }
+      
+      handleChangeEscape(event) { 
+          this.setState({stressEscape: event.target.value });
+      }
+      
+      handleChangeRelief(event) { 
+          this.setState({stressRelief: event.target.value });
+      }
+  
+      handleChangeEmotion(event) { 
+          this.setState({emotionalConseq: event.target.value });
+          }  
+  
+      handleChangePhysical(event) { 
+          this.setState({physicalConseq: event.target.value });
+      } 
+  
+      
+    handlePress =  async () => {
+      console.log("lol");
+      return fetch('http://127.0.0.1:3000/api/user/signup', 
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+          pseudo:this.state.pseudo,
+          firstname:this.state.firstname,
+          lastname:this.state.lastname
+        })
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          return json;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    handlePress2 =  async () => {
+      console.log("lol");
+      return fetch('http://127.0.0.1:3000/api/profile', 
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+        sexe:this.state.sexe,
+        age:this.state.age,
+        socialSituation : this.state.socialSituation,
+        professionalSituation : this.state.professionalSituation, 
+        children : this.state.children, 
+        stressFactors: this.state.stressFactors,
+        stressEscape: this.state.stressEscape, 
+        stressRelief:this.state.stressRelief,
+        emotionalConseq:this.state.emotionalConseq,
+        physicalConseq:this.state.physicalConseq,
+        })
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          return json;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
     render() {
       return (
     
@@ -78,33 +203,49 @@ class CreateAccount extends React.Component {
         <FormGroup>
         
        <Label  for="exampleUsername"></Label>
-       <Input className="arrondi" type="username" name="username" id="exampleUsername" placeholder="Entrez votre pseudo" />
+       <Input className="arrondi" type="username" name="username" id="exampleUsername" value={this.state.pseudo} 
+          onChange={this.handleChangePseudo.bind(this)} placeholder="Entrez votre pseudo" />
      </FormGroup>
         <FormGroup>
        
           <Label  for="exampleEmail"></Label>
           
-          <Input className="arrondi" type="email" name="email" id="exampleEmail" placeholder="Entrez votre e-mail" />
+          <Input className="arrondi" type="email" name="email" id="exampleEmail" value={this.state.email} 
+          onChange={this.handleChangeEmail.bind(this)} placeholder="Entrez votre e-mail" />
       
         </FormGroup>
         <FormGroup>
           <Label for="examplePassword"></Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="Entrez votre mot de passe" />
+          <Input type="password" name="password" id="examplePassword" value={this.state.password} 
+          onChange={this.handleChangePassword.bind(this)} placeholder="Entrez votre mot de passe" />
         </FormGroup>
        </Form>
+
+       <FormGroup >
+    <div className="float_right">
+   
+    <Button 
+    onClick={this.handlePress.bind(this)} 
+    color="secondary" 
+    size="lg" block
+    onPress={() => {
+      onSignIn().then(() => navigation.navigate("SignedIn"));
+    }}
+    >
+    </Button>
+
+    </div>
+    </FormGroup>
 
        <legend>Informations Personnelles </legend> 
 
      <Form>
 
     <FormGroup>
-    <Label  for="prenom"></Label>
-       <Input className="arrondi" type="prenom" name="prenom" id="prenom" placeholder="Entrez votre prénom" />
-    </FormGroup>
-     
-    <FormGroup>
         <Label for="sexe">Sexe</Label>
-        <Input type="select" name="sexe" id="exampleSexe">
+        <Input type="select" name="sexe" id="exampleSexe"
+          value={this.state.sexe} 
+          onChange={this.handleChangeSexe.bind(this)}>
           <option>Femme</option>
           <option>Homme</option>
         </Input>
@@ -112,12 +253,21 @@ class CreateAccount extends React.Component {
 
     <FormGroup>
     <Label  for="age">Age</Label>
-       <Input className="arrondi" type="age" name="age" id="age" placeholder="Entrez votre âge" />
+       <Input className="arrondi" type="age" name="age" id="age" value={this.state.age} 
+          onChange={this.handleChangeAge.bind(this)}> </Input>placeholder="Entrez votre âge" />
     </FormGroup>
 
     <FormGroup>
+    <Label  for="children">Enfants</Label>
+       <Input className="arrondi" type="enfants" name="enfants" id="enfants" value={this.state.children} 
+          onChange={this.handleChangeChildren.bind(this)}> </Input>placeholder="Entrez votre âge" />
+    </FormGroup>
+
+
+    <FormGroup>
     <Label  for="domaine">Domaine de Profession</Label>
-    <Input type="select" name="domaine" id="exampleDomaine">
+    <Input type="select" name="domaine" id="exampleDomaine" value={this.state.professionalSituation} 
+          onChange={this.handleChangeDomaine.bind(this)}>>
           <option>Agriculture</option>
           <option>Maintenance</option>
           <option>Ingénieurs</option>
@@ -135,15 +285,11 @@ class CreateAccount extends React.Component {
           <option>Politique, religion</option>
     </Input>    
     </FormGroup>
-
-    <FormGroup>
-    <Label  for="fonction">Fonction professionelle</Label>
-       <Input className="arrondi" type="fonction" name="fonction" id="fonctino" placeholder="Entrez votre fonction professionnelle" />
-    </FormGroup>
         
     <FormGroup>
         <Label for="source">Source</Label>
-        <Input type="select" name="source" id="exampleSource">
+        <Input type="select" name="source" id="exampleSource"> value={this.state.stressFactors} 
+          onChange={this.handleChangeFactors.bind(this)}
           <option>Familial</option>
           <option>Professionnel</option>
           <option>Financier</option>
@@ -158,56 +304,49 @@ class CreateAccount extends React.Component {
     </FormGroup>
     
     <FormGroup>
-        <Label for="timide">Vous considerez-vous comme timide ? </Label>
-        <div>
-          <CustomInput type="checkbox" id="Tpositif" label="Oui" inline />
-          <CustomInput type="checkbox" id="Tnegatif" label="Non" inline />
-        </div>
-    </FormGroup>
-       
-    <FormGroup>
         <Label for="confiance">Avez-vous confiance en vous ? </Label>
         <div>
-          <CustomInput type="checkbox" id="Coui" label="Oui" inline />
-          <CustomInput type="checkbox" id="Cnon" label="Non" inline />
+          <CustomInput type="checkbox" id="Coui" value="Oui"
+          onChange={this.handleChangeEscape.bind(this)} label="Oui" inline />
+          <CustomInput type="checkbox" id="Cnon" value="Non"
+          onChange={this.handleChangeEscape.bind(this)} label="Non" inline />
         </div>
     </FormGroup>
        
-    <FormGroup>
-        <Label for="oral">Etes-vous à l'aise à l'oral ? </Label>
-        <div>
-          <CustomInput type="checkbox" id="Ooui" label="Oui" inline />
-          <CustomInput type="checkbox" id="Onon" label="Non" inline />
-        </div>
-    </FormGroup>  
     
     <FormGroup>
         <Label for="stress">Avez-vous déjà pris un traitement contre le stress ? </Label>
         <div>
-          <CustomInput type="checkbox" id="Troui" label="Oui" inline />
-          <CustomInput type="checkbox" id="Trnon" label="Non" inline />
+          <CustomInput type="checkbox" id="Troui" value="Oui" 
+          onChange={this.handleChangeRelief.bind(this)} label="Oui" inline />
+          <CustomInput type="checkbox" id="Trnon" value="Non" 
+          onChange={this.handleChangeRelief.bind(this)} label="Non" inline />
         </div>
     </FormGroup>  
 
     <FormGroup>
         <Label for="chronique">Qualifierez-vous votre stress de chronique ? </Label>
         <div>
-          <CustomInput type="checkbox" id="Croui" label="Oui" inline />
-          <CustomInput type="checkbox" id="Crnon" label="Non" inline />
+          <CustomInput type="checkbox" id="Croui" value="Oui" 
+          onChange={this.handleChangeEmotion.bind(this)} label="Oui" inline />
+          <CustomInput type="checkbox" id="Crnon" value="Non"
+          onChange={this.handleChangeEmotion.bind(this)}  label="Non" inline />
         </div>
     </FormGroup>
        
     <FormGroup>
         <Label for="traitement">Avez-vous déjà réussi à atténuer votre stress? </Label>
         <div>
-          <CustomInput type="checkbox" id="Croui" label="Oui" inline />
-          <CustomInput type="checkbox" id="Crnon" label="Non" inline />
+          <CustomInput type="checkbox" id="Croui" value="Oui" 
+          onChange={this.handleChangePhysical.bind(this)} label="Oui" inline />
+          <CustomInput type="checkbox" id="Crnon" value="Non" 
+          onChange={this.handleChangePhysical.bind(this)} label="Non" inline />
         </div>
     </FormGroup>
 
     <FormGroup >
     <div className="float_right">
-    <Button color="secondary" size="lg" block>Valider</Button>
+    <Button onClick={this.handlePress2.bind(this)} color="secondary" size="lg" block>Valider</Button>
     </div>
     </FormGroup>
     
